04b9ba2e5815ab6fa5e79f6d48a3cd5474ebd432
heerokj <jungheerok@gmail.com>
2025-02-22 04:00:22 +0900

    fix : 로고 이미지 변경 - favicon, 메인 로고 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,11 +4,20 @@ import HeaderClient from "./Header-client";
 export default function Header() {
   return (
     <div className="flex justify-between items-center border-b-[1px] h-[64px] px-[50px] ">
-      <div className="flex gap-8">
-        <Link href={"/"}>로고</Link>
-        <Link href={"/feed"}>피드</Link>
-        <Link href={"/my-books"}>내 서재</Link>
-        <Link href={"/chatting"}>챗챗</Link>
+      <div className="flex gap-[50px]">
+        <Link href={"/"}>
+          <img
+            src="/images/bookkki-icon.png"
+            alt="bookkki-icon"
+            height={40}
+            width={40}
+          />
+        </Link>
+        <div className="flex gap-[50px] mt-[10px]">
+          <Link href={"/feed"}>피드</Link>
+          <Link href={"/my-books"}>내 서재</Link>
+          <Link href={"/chatting"}>챗챗</Link>
+        </div>
       </div>
       <HeaderClient />
     </div>
