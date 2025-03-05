6e6a529c533bf0643e27a355fcf9db28cb2b5a84
heerokj <jungheerok@gmail.com>
2025-03-05 19:31:14 +0900

    fix : 이미지가 div태그 밖으로 나가는 문제 수정

--- src/components/feed/FeedCard.tsx
+++ src/components/feed/FeedCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FeedData } from "@/types/feed";
 import Link from "next/link";
 import Profile from "../Profile";
@@ -6,25 +7,33 @@ export default function FeedCard(data: FeedData) {
   return (
     <>
       {data ? (
-        <div key={data.id} className="feed-item border-[1px] rounded-sm">
+        <div
+          key={data.id}
+          className="feed-item border-[1px] rounded-sm shadow-sm"
+        >
           <div className="flex justify-between p-2">
             <div className="flex gap-2">
               <Profile />
               <div>nickname</div>
             </div>
-            <button>
+            <button onClick={() => alert("준비중입니다.")}>
               <img src="/icons/ellipsis.svg" alt="ellipsis" width={15} />
             </button>
           </div>
           <Link href={`/feed/${data.id}`}>
-            <div>
+            <div className="overflow-hidden">
               {data.image_urls?.map((img) => (
-                <div key={img}>
-                  <img src={img} alt="img" className="object-fill" />
+                <div key={img} className="h-[300px]">
+                  <img
+                    src={img}
+                    alt="img"
+                    height={250}
+                    className="h-full w-full object-cover"
+                  />
                 </div>
               ))}
             </div>
-            <div className="p-2">
+            <div className="h-[80px] p-2">
               <div>{data.title}</div>
               <div className="text-[10px]">{data.created_at}</div>
             </div>
