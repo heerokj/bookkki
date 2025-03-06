963b6093ff6f3e49985401f4dc07a5010bfa6c9c
heerokj <jungheerok@gmail.com>
2025-03-07 01:26:33 +0900

    feat : 댓글 삭제 구현

--- src/components/feedDetail/FeedContent.tsx
+++ src/components/feedDetail/FeedContent.tsx
@@ -4,6 +4,8 @@ import FeedContentProfileContainer from "./FeedContentProfileContainer";
 import FeedContentLikeContainer from "./FeedContentLikeContainer";
 import CommentEditor from "./CommentEditor";
 import { useRef, useState } from "react";
+import Profile from "../Profile";
+import { deleteComment } from "@/hooks/fetchComments";
 export default function FeedContent({
   feedData,
   commentDataList,
@@ -14,6 +16,12 @@ export default function FeedContent({
   const focusTest = useRef<HTMLInputElement>(null);
   const [comments, setComments] = useState<FeedComment[]>(commentDataList);
 
+  const handleCommentDelete = async (commentId: string, postId: string) => {
+    alert("삭제하시겠습니까?");
+    const data = await deleteComment(commentId, postId);
+    setComments(data?.commentDataList as FeedComment[]);
+  };
+
   return (
     <div className="feed-contents flex flex-col justify-between border-[1px] rounded-sm">
       <FeedContentProfileContainer />
@@ -21,14 +29,25 @@ export default function FeedContent({
         <div className="pb-4">{feedData.content}</div>
         <div>
           {comments.map((comment) => (
-            <div key={comment.id} className="flex gap-4 py-2">
-              <div>프로필</div>
-              <div>
-                <div className="flex gap-2">
-                  <span className="text-[#313d44] font-bold">heerok</span>
-                  <div>{comment.comment}</div>
+            <div key={comment.id} className="flex justify-between py-2">
+              <div className="flex gap-2">
+                <Profile />
+                <div>
+                  <div className="flex gap-2">
+                    <span className="text-[#313d44] font-bold">heerok</span>
+                    <div>{comment.comment}</div>
+                  </div>
+                  <div>{comment.created_at}</div>
                 </div>
-                <div>{comment.created_at}</div>
+              </div>
+              <div className="text-[12px]">
+                <button className="p-[2px]">수정</button>
+                <button
+                  className="p-[2px]"
+                  onClick={() => handleCommentDelete(comment.id, feedData.id)}
+                >
+                  삭제
+                </button>
               </div>
             </div>
           ))}
