e00a8b9a82ed877d52bcadb501974b69915912e1
heerokj <jungheerok@gmail.com>
2025-03-06 21:51:17 +0900

    fix : 메인 화면 로그인 버튼 수정

--- src/components/layout/Header-client.tsx
+++ src/components/layout/Header-client.tsx
@@ -12,8 +12,13 @@ export default function HeaderClient() {
     <div className="flex gap-4">
       {/* <span>아래 Profile은 임시프로필</span> */}
       <Profile />
-      <button onClick={onclickSignIn}>로그인</button>
-      <button>다크모드</button>
+      <img src="/icons/moon.svg" alt="moon" height={20} width={20} />
+      <button
+        onClick={onclickSignIn}
+        className="p-[7px] px-[13px] rounded-md bg-[#84bbe1] hover:bg-[#00bbf9] text-white text-[14px]"
+      >
+        로그인
+      </button>
     </div>
   );
 }
