80b937c3306130bf230340f6a49216b3011c99e3
heerokj <jungheerok@gmail.com>
2025-03-10 23:16:36 +0900

    add :  상수 파일 추가

---
+++ src/shared/constants/header.ts
@@ -0,0 +1,18 @@
+export const HEADER_LISTS = [
+  {
+    text: "홈",
+    href: "/",
+  },
+  {
+    text: "피드",
+    href: "/feed",
+  },
+  {
+    text: "채팅",
+    href: "/chat",
+  },
+  {
+    text: "내 서재",
+    href: "/my-books",
+  },
+];
