d8a573da4aa095b400e7cca8621d537c295c56d9
heerokj <jungheerok@gmail.com>
2025-03-14 01:17:39 +0900

    feat : 로그인 구현

--- src/auth.ts
+++ src/auth.ts
@@ -13,10 +13,44 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Kakao from "next-auth/providers/kakao";
 import Naver from "next-auth/providers/naver";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { createClient } from "./utils/supabase/server";
+import bcrypt from "bcryptjs";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Kakao, Naver],
+  providers: [
+    GitHub,
+    Kakao,
+    Naver,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        userID: { label: "userID", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const supabase = await createClient();
+        //authorize 함수에서 유저가 입력한 아이디, 패스워드를 받음
+        const { userID, password } = credentials;
+
+        //Supabase에서 해당 아이디의 유저를 조회
+        const { data: loginUser, error } = await supabase
+          .from("users")
+          .select()
+          .eq("user_id", userID)
+          .single();
+
+        if (error || !loginUser) throw new Error("User not found");
+
+        //패스워드 검증
+        const isValid = bcrypt.compare(password as string, loginUser.password);
+        if (!isValid) throw new Error("Invalid credentials");
+
+        // 성공 시 유저 정보 반환
+        return { id: loginUser.user_id, nickname: loginUser.nickname };
+      },
+    }),
+  ],
   callbacks: {
     async signIn({ user, account }) {
       const supabase = await createClient();
