7aec4500953dfef5fd958c1a4d57fb05e71bdd29
heerokj <jungheerok@gmail.com>
2025-03-13 21:07:04 +0900

    feat : 소셜로그인 네이버, 카카오 구현

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,5 @@
 // nextAuth의 configuration
 // NextAuth 설정 및 인증 함수 제공
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Kakao from "next-auth/providers/kakao";
-import Naver from "next-auth/providers/naver";
-
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Kakao, Naver],
-});
 
 // NOTE - 역할 정리
 // NextAuth()를 호출하여 인증 설정을 만듦
@@ -16,3 +8,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 // handlers: GET과 POST 요청을 처리하는 핸들러 (API 라우트에서 사용됨)
 // signIn: 로그인 요청 함수
 // signOut: 로그아웃 요청 함수
+
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Kakao from "next-auth/providers/kakao";
+import Naver from "next-auth/providers/naver";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [GitHub, Kakao, Naver],
+});
