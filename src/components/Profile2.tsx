36675138082f541e45642d48d7c19598a5c2030e
heerokj <jungheerok@gmail.com>
2025-04-01 15:31:48 +0900

    del: 불필요한 파일 삭제 add : 필요한 아이콘 추가

--- src/components/Profile2.tsx
+++
@@ -1,9 +0,0 @@
-import Avatar from "boring-avatars";
-//임시 프로필
-export default function Profile2() {
-  return (
-    <div>
-      <Avatar name="Sacagawea" variant="beam" size={30} />
-    </div>
-  );
-}
