55559de14839354b86445fdaed22b7fe4384ddf8
heerokj <jungheerok@gmail.com>
2025-06-17 01:11:10 +0900

    Fix : 공통 레이아웃 fallback 수정

--- src/app/(main)/(with-navigation)/layout.tsx
+++ src/app/(main)/(with-navigation)/layout.tsx
@@ -1,13 +1,13 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
-import LoadingSkeleton from "@/components/skeleton/LoadingSkeleton";
+
 import { ReactNode, Suspense } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
       <Header />
-      <Suspense fallback={<LoadingSkeleton />}>{children}</Suspense>
+      <Suspense fallback={<div>로딩...</div>}>{children}</Suspense>
       <Footer />
     </div>
   );
