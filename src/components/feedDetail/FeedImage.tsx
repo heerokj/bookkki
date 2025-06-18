1a42b512227dfc91e39e3affe8ecc417606d097e
heerokj <jungheerok@gmail.com>
2025-06-18 17:11:16 +0900

    Refactor : 피드디테일 페이지 이미지 최적화

--- src/components/feedDetail/FeedImage.tsx
+++ src/components/feedDetail/FeedImage.tsx
@@ -1,11 +1,18 @@
 import { FeedData } from "@/types/feed";
+import Image from "next/image";
 
 export default function FeedImage({ feedImage }: { feedImage: FeedData }) {
   return (
     <div className="feed-image w-full h-full overflow-hidden">
       {feedImage.image_urls?.map((img) => (
-        <div key={img} className="w-full h-full">
-          <img src={img} alt="img" className="w-full h-full object-fill" />
+        <div key={img} className="h-full">
+          <Image
+            src={img}
+            height={250}
+            width={300}
+            alt="img"
+            className="w-full h-full"
+          />
         </div>
       ))}
     </div>
