59389f51e1384cfbba02c48379ae32b5eec23b58
heerokj <jungheerok@gmail.com>
2025-06-19 22:21:33 +0900

    Feat : 메인페이지 캐러셀 적용

--- src/app/(main)/(with-navigation)/(with-searching)/page.tsx
+++ src/app/(main)/(with-navigation)/(with-searching)/page.tsx
@@ -1,30 +1,9 @@
-import { auth } from "@/auth";
-import Link from "next/link";
+import Carousel from "@/components/main/Carousel";
 
 export default async function Home() {
-  const session = await auth();
-
-  if (session?.user) {
-    return (
-      <div>
-        <div className="text-[30px]">로그인 상태</div>
-      </div>
-    );
-  }
-
   return (
-    <div className="h-[200px]">
-      <div>
-        <h3>따끈따끈 신작</h3>
-        <Link href={"/"}></Link>
-        <p>새로 나온 책들을 만나보세요!</p>
-        <div>
-          <ul>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-      </div>
+    <div>
+      <Carousel />
     </div>
   );
 }
