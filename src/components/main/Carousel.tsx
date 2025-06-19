4c9679de12ffa8c5d46f356102479051c76186f0
heerokj <jungheerok@gmail.com>
2025-06-19 22:22:06 +0900

    Fix : 캐러셀에 외부 링크 적용

--- src/components/main/Carousel.tsx
+++ src/components/main/Carousel.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import EmblaCarousel from "../carousel/EmblaCarousel";
 import { EmblaOptionsType } from "embla-carousel";
+import banner1 from "/public/images/carousel-1.png";
+import banner2 from "/public/images/carousel-2.png";
+import banner3 from "/public/images/carousel-3.png";
 
 const OPTIONS: EmblaOptionsType = { loop: true };
 const SLIDES = [
-  "images/carousel-1.png",
-  "images/carousel-2.png",
-  "images/carousel-3.png",
+  { id: "1", img: banner1, link: "https://sibf.or.kr" },
+  { id: "2", img: banner2, link: "/feed" },
+  { id: "3", img: banner3, link: "/" },
 ];
 
 export default function Carousel() {
