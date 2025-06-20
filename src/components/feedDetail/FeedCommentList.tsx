f2e5a5feb053367ca3e6a3f4eb0f21d297a65899
heerokj <wjdgmlfhr0321@gmail.com>
2025-06-21 02:19:06 +0900

    Feat : 수정버튼 클릭 시 input 활성화

--- src/components/feedDetail/FeedCommentList.tsx
+++ src/components/feedDetail/FeedCommentList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Profile from "../common/Profile";
 import { getDistanceToNow } from "@/shared/utils/Date/date";
 import { FeedComment, FeedData } from "@/types/feed";
@@ -18,52 +18,96 @@ export default function FeedCommentList({
   comments: FeedComment[];
   setComments: React.Dispatch<React.SetStateAction<FeedComment[]>>;
 }) {
-  const handleCommentDelete = async (commentId: string, postId: string) => {
+  const [editTargetId, setEditTargetId] = useState<string | null>(null);
+  const [editComment, setEditComment] = useState("");
+  const inputRef = useRef(null);
+
+  // 저장버튼
+  const handleClickSave = async () => {};
+
+  //취소버튼
+  const handleClickCancel = (id: string) => {
+    if (editTargetId !== id) return;
+    setEditTargetId(null);
+    setEditComment("");
+  };
+
+  // 수정버튼
+  const handleClickUpdate = (id: string, comment: string) => {
+    setEditTargetId(id);
+    setEditComment(comment);
+  };
+
+  // 삭제버튼
+  const handleClickDelete = async (commentId: string, postId: string) => {
     const isConfirmed = window.confirm("삭제하시겠습니까?");
     if (!isConfirmed) return;
     const data = await deleteComment(commentId, postId);
     setComments(data?.commentDataList as FeedComment[]);
   };
 
-  const handleCommentUpdate = async () => {};
-
   return (
-    <div className="comments-section px-6 h-[280px] overflow-y-auto">
+    <div className="comments-section px-4 pb-4 h-[280px] overflow-y-auto">
       <div className="p-4">{feedData.content}</div>
       <div>
-        {comments.map((comment) => (
-          <div key={comment.id} className="flex justify-between py-2">
-            {/* 왼쪽 부분 */}
-            <div className="flex gap-2 flex-[8]">
-              <Profile info={comment.users} />
-              <div>
-                <div className="flex gap-2 items-center">
-                  <p className="text-[#313d44] font-bold">
-                    {comment.users?.nickname}
-                  </p>
-                  <p className="text-[10px] text-gray-500">
-                    {getDistanceToNow(comment.created_at)}
-                  </p>
+        {comments.map((comment) => {
+          const isEditing = comment.id === editTargetId;
+          return (
+            <div key={comment.id} className="flex justify-between py-2">
+              {/* 왼쪽 부분 */}
+              <div className="flex gap-2 flex-[8]">
+                <Profile info={comment.users} />
+                <div>
+                  <div className="flex gap-2 items-center">
+                    <p className="text-[#313d44] font-bold">
+                      {comment.users?.nickname}
+                    </p>
+                    <p className="text-[10px] text-gray-500">
+                      {getDistanceToNow(comment.created_at)}
+                    </p>
+                  </div>
+
+                  {isEditing ? (
+                    <input
+                      ref={inputRef}
+                      value={editComment}
+                      onChange={(e) => setEditComment(e.target.value)}
+                    />
+                  ) : (
+                    <div>{comment.comment}</div>
+                  )}
                 </div>
-                <div>{comment.comment}</div>
               </div>
+
+              {/* 오른쪽 부분 */}
+              {comment.users?.nickname === userData?.nickname && (
+                <div className="button-bundle text-sm flex-[1] pl-2">
+                  <button
+                    className="p-[2px]"
+                    onClick={() =>
+                      isEditing
+                        ? handleClickSave()
+                        : handleClickUpdate(comment.id, comment.comment)
+                    }
+                  >
+                    {isEditing ? "저장" : "수정"}
+                  </button>
+
+                  <button
+                    className="p-[2px]"
+                    onClick={() =>
+                      isEditing
+                        ? handleClickCancel(comment.id)
+                        : handleClickDelete(comment.id, feedData.id)
+                    }
+                  >
+                    {isEditing ? "취소" : "삭제"}
+                  </button>
+                </div>
+              )}
             </div>
-            {/* 오른쪽 부분 */}
-            {comment.users?.nickname === userData?.nickname && (
-              <div className="button-bundle text-[11px] flex-[1] pl-2">
-                <button className="p-[2px]" onClick={handleCommentUpdate}>
-                  수정
-                </button>
-                <button
-                  className="p-[2px]"
-                  onClick={() => handleCommentDelete(comment.id, feedData.id)}
-                >
-                  삭제
-                </button>
-              </div>
-            )}
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
