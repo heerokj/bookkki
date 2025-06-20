41aff798a63761ef25cdeff930d80630e046efa5
heerokj <wjdgmlfhr0321@gmail.com>
2025-06-20 18:39:31 +0900

    Feat : 유저 타입 파일

---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  email: string;
+  nickname: string;
+  profile_url: string;
+  user_id: string;
+  created_at: string;
+  id: string;
+  password: string;
+  provider: string;
+}
