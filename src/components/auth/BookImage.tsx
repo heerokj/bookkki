485a85d0c9ec7c6c59346181e55f797c4abe53c5
heerokj <wjdgmlfhr0321@gmail.com>
2025-06-24 14:59:54 +0900

    Refactor : 로그인 및 회원가입 페이지 이미지 최적화

--- src/components/auth/BookImage.tsx
+++ src/components/auth/BookImage.tsx
@@ -1,9 +1,12 @@
+import bookkki from "@/../public/images/bookkki-main-img.png";
+import Image from "next/image";
+
 export default function BookImage() {
   return (
     <div className="left-area col-span-2 h-screen">
-      <img
-        src="images/bookkki-main-img.png"
-        alt=""
+      <Image
+        src={bookkki}
+        alt="북끼 이미지"
         className="w-full h-full object-fit"
       />
     </div>
