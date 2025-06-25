e8c9fab13957797874bbf98250bd49dbab6a8f58
heerokj <wjdgmlfhr0321@gmail.com>
2025-06-25 19:22:40 +0900

    Fix : 메타데이터 수정(openGraph 추가 )

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,16 @@ import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 export const metadata: Metadata = {
   title: "북끼",
-  description: "도서를 검색하고 읽은 책을 공유하는 서비스입니다.",
+  description: "텍스트 힙! 도서를 검색하고 읽은 책을 북끼와 공유해봐요!",
+  openGraph: {
+    title: "북끼",
+    description: "텍스트 힙! 도서를 검색하고 읽은 책을 북끼와 공유해봐요!",
+    images: "https://bookkki.vercel.app/opengraph-image.png",
+    url: "https://bookkki.vercel.app",
+    siteName: "Bookkki | 북끼",
+    locale: "ko_KR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
