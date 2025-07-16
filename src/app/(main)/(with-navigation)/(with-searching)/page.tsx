369f5e9b378aec112f9266ba371617c35189d12e
heerokj <wjdgmlfhr0321@gmail.com>
2025-07-16 19:42:34 +0900

    메인 캐러셀 수정(스타일 적용 전)

--- src/app/(main)/(with-navigation)/(with-searching)/page.tsx
+++ src/app/(main)/(with-navigation)/(with-searching)/page.tsx
@@ -1,9 +1,9 @@
-import Carousel from "@/components/main/Carousel";
+import MainCarousel from "@/components/main/MainCarousel";
 
 export default async function Home() {
   return (
     <div>
-      <Carousel />
+      <MainCarousel />
     </div>
   );
 }
---
+++ src/components/main/MainCarousel.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import EmblaCarousel from "../carousel/EmblaCarousel";
+import banner1 from "/public/images/carousel-1.png";
+import banner2 from "/public/images/carousel-2.png";
+import banner3 from "/public/images/carousel-3.png";
+import Image from "next/image";
+import Link from "next/link";
+
+const mainSlides = [
+  { img: banner1, link: "https://sibf.or.kr" },
+  { img: banner2, link: "/feed" },
+  { img: banner3, link: "/" },
+];
+
+export default function MainCarousel() {
+  return (
+    <div className="mb-6">
+      <EmblaCarousel
+        slides={mainSlides}
+        options={{ loop: true }}
+        renderSlide={(slide) => (
+          <Link href={slide.link}>
+            <Image
+              src={slide.img}
+              height={250}
+              width={300}
+              alt="img"
+              quality={100}
+              className="w-full h-full"
+            />
+          </Link>
+        )}
+      />
+    </div>
+  );
+}
