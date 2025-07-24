d5262bb3b86407976cb98f4620f603d1aa4eb30a
heerokj <wjdgmlfhr0321@gmail.com>
2025-07-24 22:07:28 +0900

    Feat : 메인 페이지 SectionBlock 적용

--- src/app/(main)/(with-navigation)/(with-searching)/page.tsx
+++ src/app/(main)/(with-navigation)/(with-searching)/page.tsx
@@ -1,9 +1,25 @@
-import MainCarousel from "@/components/main/MainCarousel";
+import BookCafeList from "@/components/main/BookCafeList";
+import RecommendBookList from "@/components/main/RecommendBookList";
+import SectionBlock from "@/components/main/SectionBlock";
 
 export default async function Home() {
   return (
     <div>
-      <MainCarousel />
+      <SectionBlock title="최신순">
+        <div>1</div>
+      </SectionBlock>
+      <SectionBlock
+        title="북끼의 추천"
+        description="제가 추천해준 책 읽어보실래요? 2025년 07년 22일 업데이트"
+      >
+        <RecommendBookList />
+      </SectionBlock>
+      <SectionBlock
+        title="북카페 추천"
+        description="분위기 좋은 카페에서 책 읽으면 얼마나 기분이 좋게요"
+      >
+        <BookCafeList />
+      </SectionBlock>
     </div>
   );
 }
