e3edcbfefad60a34fafa2900da4005b600146724
heerokj <wjdgmlfhr0321@gmail.com>
2025-07-26 18:39:27 +0900

    Style : 체팅 스켈레톤 스타일 수정

--- src/components/skeleton/ChatListSkeleton.tsx
+++ src/components/skeleton/ChatListSkeleton.tsx
@@ -3,11 +3,11 @@ import SkeletonBox from "./SkeletonBox";
 export default function ChatListSkeleton() {
   return (
     <>
-      <div className="border-2 rounded-md w-60 h-36 flex flex-col p-2 justify-center items-center">
+      <div className="border-2 rounded-md w-60 h-36 flex flex-col p-6 justify-between items-center">
         <SkeletonBox width="w-[150px]" height="h-[20px]" rounded="md" />
         <SkeletonBox
-          width="w-[100px]"
-          height="h-[40px]"
+          width="w-[150px]"
+          height="h-[30px]"
           rounded="md"
           className="mt-[30px] p-[7px]"
         />
