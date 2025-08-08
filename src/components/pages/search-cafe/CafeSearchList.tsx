0d8ff40f5fdb70aece363e9e8f8fa00a2bd6cd46
heerokj <wjdgmlfhr0321@gmail.com>
2025-08-08 23:10:21 +0900

    Feat : 기본데이터 가져오기 및 검색일 경우에만 하단 '더보기' 문구보임

--- src/components/pages/search-cafe/CafeSearchList.tsx
+++ src/components/pages/search-cafe/CafeSearchList.tsx
@@ -2,31 +2,48 @@
 import React, { useEffect } from "react";
 import CafeCard from "./CafeCard";
 import KaKaoMap from "./KaKaoMap";
-import { useGetSearchCafe } from "@/hooks/use-cafe";
+import { useGetInitialCafe, useGetSearchCafe } from "@/hooks/use-cafe";
 import { useInView } from "react-intersection-observer";
 
 export default function CafeSearchList({ searchText }: { searchText: string }) {
   const [ref, inView] = useInView();
+  const isSearching = Boolean(searchText?.trim()); // 검색 중인지 여부 판단
+
+  const {
+    data: initialData,
+    isLoading: isInitialLoading,
+    isError: isInitialError,
+  } = useGetInitialCafe();
+
   // 검색 시 데이터 불러오기
-  const { data, isLoading, error, fetchNextPage, hasNextPage } =
-    useGetSearchCafe(searchText);
-  const searchCafe = data?.pages.flatMap((page) => page?.data) ?? [];
+  const {
+    data,
+    isLoading: isSearchLoading,
+    isError: isSearchError,
+    fetchNextPage,
+    hasNextPage,
+  } = useGetSearchCafe(searchText);
+
+  const searchData = data?.pages.flatMap((page) => page?.data) ?? [];
 
   useEffect(() => {
+    if (!isSearching) return;
     if (inView && hasNextPage) {
       fetchNextPage();
     }
-  }, [inView, hasNextPage, fetchNextPage]);
+  }, [inView, hasNextPage, fetchNextPage, isSearching]);
 
-  if (isLoading) return <span>로딩중입니다...</span>;
-  if (error) return <span>오류가 발생했습니다 : {error.message}</span>;
+  if (isInitialLoading) return <span>데이터 가져오는 중...</span>;
+  if (isInitialError) return <span>오류가 발생했습니다</span>;
+  if (isSearchLoading) return <span>검색중...</span>;
+  if (isSearchError) return <span>검색 중 오류가 발생했습니다</span>;
 
   return (
     <div className="search-wrap mt-7">
-      <div className="search-result flex gap-6 h-[660px]">
+      <div className="search-result flex gap-6 h-[730px]">
         <div className="search-result-list w-1/2 overflow-y-auto">
           <div className="flex flex-col gap-4 ">
-            {searchCafe.map((cafe, index) => (
+            {initialData?.map((cafe, index) => (
               <CafeCard
                 key={`${cafe.CNTC_RESRCE_NO}-${index}`}
                 title={cafe.TITLE}
@@ -34,11 +51,17 @@ export default function CafeSearchList({ searchText }: { searchText: string }) {
               />
             ))}
           </div>
-          <div ref={ref} className="text-center py-[40px]">
-            {hasNextPage ? (
-              <div>더보기</div>
-            ) : (
-              <div>모든 피드를 불러왔습니다.</div>
+          <div ref={ref} className="text-center pt-4">
+            {isSearching && (
+              <div ref={ref} className="text-center">
+                {isSearchLoading ? (
+                  <div>로딩중...</div>
+                ) : hasNextPage ? (
+                  <div>더보기</div>
+                ) : (
+                  <div>모든 피드를 불러왔습니다.</div>
+                )}
+              </div>
             )}
           </div>
         </div>
@@ -47,3 +70,9 @@ export default function CafeSearchList({ searchText }: { searchText: string }) {
     </div>
   );
 }
+
+/*
+1. 첫 진입 시 데이터는 기본 데이터 (5개) - 더보기, 모든 피드를 불러왔습니다. 문구 없음
+2. 검색하면 검색데이터 보여줌 & 기본 데이터는 사라짐
+3. 검색바에 검색에 지우면 다시 기본데이터 보여줌
+*/
