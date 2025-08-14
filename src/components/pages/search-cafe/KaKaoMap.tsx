47e193ee53d65df05940c9332e032a9e4956d068
heerokj <wjdgmlfhr0321@gmail.com>
2025-08-14 17:49:25 +0900

    Feat :  지도 초기 위치 설정

--- src/components/pages/search-cafe/KaKaoMap.tsx
+++ src/components/pages/search-cafe/KaKaoMap.tsx
@@ -1,26 +1,44 @@
 "use client";
 import useKakaoLoader from "@/hooks/useKakaoLoader";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState } from "react";
+import { CafeData } from "@/types/cafe";
 
-export default function KaKaoMap() {
+// type Props = {
+//   initialData: CafeData;
+//   isSearchData: CafeData;
+// };
+
+export default function KaKaoMap({ initialCafe }: { initialCafe: CafeData }) {
+  const [location, setLocation] = useState(
+    convertToLatLng(initialCafe.COORDINATES)
+  );
+
   useKakaoLoader();
 
+  // useEffect(() => {}, [location]);
+
   return (
-    <div className="search-result-map border-2 w-1/2 ">
+    <div className="search-result-map border-2 w-1/2">
       <Map // 지도를 표시할 Container
         id="map"
-        center={{
-          // 지도의 중심좌표
-          lat: 33.450701,
-          lng: 126.570667,
-        }}
+        center={location}
         style={{
           // 지도의 크기
           width: "100%",
           height: "100%",
         }}
         level={3} // 지도의 확대 레벨
-      />
+      >
+        <MapMarker position={location}>
+          <div style={{ color: "#000" }}>카페상호넣을예정</div>
+        </MapMarker>
+      </Map>
     </div>
   );
+}
+
+function convertToLatLng(str: string) {
+  const [lat, lng] = str.split(" ").map(Number);
+  return { lat, lng };
 }
