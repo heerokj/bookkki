315a985cd1a10fdf2dc4ba50ccd6920c2ab0e0de
heerokj <wjdgmlfhr0321@gmail.com>
2025-08-14 18:13:55 +0900

    Fix : 이미지 오류 수정

--- src/components/pages/search-cafe/CafeCard.tsx
+++ src/components/pages/search-cafe/CafeCard.tsx
@@ -11,13 +11,14 @@ export default function CafeCard({
 }) {
   return (
     <div className="search-cafe-card flex gap-4 border-2 rounded-lg p-2 ">
-      <div className="h-[100px] w-[150px]">
+      <div className="h-[105px] w-[150px]">
         <Image
           src={cafeImg}
           width={150}
           height={100}
           alt="book"
           className="w-full h-full rounded-md"
+          priority
         />
       </div>
       <div className="cafe-info pt-2">
